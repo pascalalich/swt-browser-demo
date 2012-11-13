cfe3d721ded5de56cbbab2370eb685c0f028248d
Pascal Alich <pascal.alich@gmx.de>
2012-11-13 21:15:08 +0100

    Cleanup JavaScript files

--- com.zuehlke.browser.zwibbler/html/js/initZwibbler.js
+++ com.zuehlke.browser.zwibbler/html/js/initZwibbler.js
@@ -1,12 +1,3 @@
-
-
 document.getElementsByTagName("body")[0].style.margin="0px";
 zwibbler.getFrame().width = %s;
 zwibbler.getFrame().height = %s;
-
-/*
-$("canvas").click(function() { 
-	// drawingChanged is injected via a SWT BrowserFunction
-	drawingChanged();
-});
-*/
--- com.zuehlke.browser.zwibbler/html/js/saveZwibbler.js
+++ com.zuehlke.browser.zwibbler/html/js/saveZwibbler.js
@@ -1,13 +1,1 @@
 zwibbler.saveToString(saveZwibbler);
-
-/*
-app.saveClicked();
-
-var wait = setInterval(function(){
-	$("#savefilename").val("download");
-	$("#savetype").val("client");
-	Dialog.current().okayClicked();
-    
-    clearInterval(wait);
-}, 1000);
-*/
